41dadb33b6ffc513e8e067d934d5cac4cccc6e07
Nikola <88djony@gmail.com>
2021-06-16 10:11:41 -0700

    js files created and added functions

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,4 +1,4 @@
-const { Comment } = require('../module');
+const { Comment } = require('../models');
 
 const commentData = [
     {
--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session(sees));
+app.use(session(sess));
 
 // path to the routes
 app.use(routes);
