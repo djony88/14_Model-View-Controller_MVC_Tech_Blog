6fe70118737a64b3983d899b2421b0e5571eee06
Nikola <88djony@gmail.com>
2021-06-16 09:07:38 -0700

    comment seeds added

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,38 @@
+const { Comment } = require('../module');
+
+const commentData = [
+    {
+        comment_text: "I thought you are talking about coffee... Just kidding... Didn't know that, thanks!",
+        post_id: 3,
+        user_id: 1 
+    },
+    {
+        comment_text: "Definitely one of the go to for me...",
+        post_id: 1,
+        user_id: 4
+    },
+    {
+        comment_text: "This is a great one and saved me a lot of time and unnecessary features for sure.",
+        post_id: 4,
+        user_id: 2
+    },
+    {
+        comment_text: "I am using this one all the time!",
+        post_id: 4,
+        user_id: 3
+    },
+    {
+        comment_text: "Didn't know about this one. Thank you!!!?",
+        post_id: 5,
+        user_id: 5
+    },
+    {
+        comment_text: "After testing this one I am using it all the time. It works great for me.",
+        post_id: 5,
+        user_id: 4
+      },
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
